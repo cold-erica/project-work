889ac5ae22c35dbe6c49cc777e0f37e408102210
taiye <ts.taiye@gmail.com>
2019-11-16 01:26:15 +0300

    added swal

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import { LikeHandler } from './like';
 import { Slider } from './slider';
+import Swal from 'sweetalert2';
 
 
 class Application {
@@ -10,8 +11,35 @@ class Application {
     start() {
         this.slider = new Slider();
         this.slider.start();
-
+        this.setupFeedbackForm();
         new LikeHandler();
+    }
+
+    setupFeedbackForm() {
+        const el = document.querySelector('.js-form-button');
+        el.addEventListener('click', (e) => {
+            const name = document.querySelector('.js-form-name').value;
+            const email = document.querySelector('.js-form-email').value;
+            const contact = document.querySelector('.js-form-contact').value;
+
+            if (!name || !email || !contact) {
+                Swal.fire({
+                    title: 'Error!',
+                    text: 'Not all fields filled',
+                    icon: 'error',
+                    confirmButtonText: 'Got it'
+                });
+                return;
+            }
+
+            Swal.fire({
+                title: 'Success!',
+                text: `Hello ${name}, your feedback is sent to our support wait an answer to ${email}`,
+                icon: 'success',
+                confirmButtonText: 'Cool'
+            });
+
+        })
     }
 
 }
